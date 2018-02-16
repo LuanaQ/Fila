2903117f3732a0d13211cfd5134c7a6825e47eaa
LuanaQ <31023070+LuanaQ@users.noreply.github.com>
2018-02-16 14:32:42 -0200

    Update AlpliçaoFila.java

--- AlpliçaoFila.java
+++ AlpliçaoFila.java
@@ -2,8 +2,8 @@ public class AlpliçaoFila{
 	public static void main(String args[]){
 		Fila fila = new Fila();
 		fila.inserir(10);
-		fila.inserir(21);
-		fila.inserir(32);
+		fila.inserir(20);
+		fila.inserir(30);
 		fila.inserir(40);
 
 		while (!fila.isEmpty() ){
